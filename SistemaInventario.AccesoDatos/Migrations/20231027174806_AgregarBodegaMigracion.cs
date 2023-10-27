056e539519d8b1d889a1fb1251808285fcba67b3
gustavo22cig <gustavo22cig@gmail.com>
2023-10-27 13:52:57 -0400

    configuracion del proyecto

---
+++ SistemaInventario.AccesoDatos/Migrations/20231027174806_AgregarBodegaMigracion.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaInventario.AccesoDatos.Migrations
+{
+    public partial class AgregarBodegaMigracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bodegas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<int>(type: "int", maxLength: 60, nullable: false),
+                    Descripcion = table.Column<int>(type: "int", maxLength: 100, nullable: false),
+                    Estado = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bodegas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bodegas");
+        }
+    }
+}
